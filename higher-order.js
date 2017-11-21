57ec7b20898ccbd8c209065347f99dda8d55b9c2
CADBOT <charliec364@gmail.com>
2017-11-21 09:17:29 -0800

    add

---
+++ higher-order.js
@@ -0,0 +1,48 @@
+foods = [
+  {
+    name: 'burger', 
+    ingredients: [
+      'beef', 'bun', 'lettuce', 'ketchup'
+    ]
+  },
+  {
+    name: 'fries', 
+    ingredients: [
+      'potatoes', 'oil'
+    ]
+  },
+  {
+    name: 'chicken sandwich', 
+    ingredients: [
+      'chicken', 'lettuce', 'mayo', 'bun'
+    ]
+  },
+  {
+    name: 'tea', 
+    ingredients: [
+      'tea'
+    ]
+  },
+  {
+    name: 'veggie burger', 
+    ingredients: [
+      'veggies', 'bun'
+    ]
+  },
+  {
+    name: 'veggie burger', 
+    ingredients: [
+      'veggies', 'bun'
+    ]
+  },
+  {
+    name: 'pancakes', 
+    ingredients: [
+      'wheat', 'syrup'
+    ]
+  }
+]
+
+function veggieFoodFilter() {
+
+}
